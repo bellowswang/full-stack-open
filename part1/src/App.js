60ecfbb58a1e44ed780b7a55204cf7e538dfa4e8
Yihong Wang <bellowswang@gmail.com>
2023-03-13 22:37:56 +0100

    submit 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,17 +5,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(updatedNeutral + good + bad)
+  }
+
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(updatedBad + good + neutral)
+  }
+
+  const average = (good * 1 + neutral * 0 + bad * (-1)) / all
+
+  const positive = good / all * 100
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
