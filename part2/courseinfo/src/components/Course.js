d0a70ba8a2e8a5f2d612ecdb89ce35af05cd4bdd
Yihong Wang <bellowswang@gmail.com>
2023-03-19 22:58:21 +0100

    submit 2.4 and 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,54 +1,59 @@
 const Course = (props) => {
 
-    const {course} = props
+    const { courses } = props
 
     const Header = (props) => {
-      const {name} = props
-      return (
-        <h1>{name}</h1>
-      )
+        const { name } = props
+        return (
+            <h2>{name}</h2>
+        )
     }
-  
+
     const Part = (props) => {
-      const {part, exercise} = props
-      return (
-        <p> {part} {exercise} </p>
-      )
+        const { part, exercise } = props
+        return (
+            <p> {part} {exercise} </p>
+        )
     }
-  
+
     const Content = (props) => {
-      const {parts} = props
-      return (
-        <div>
-          {parts.map((cr, i) => 
-              <Part key={i} part={cr.name} exercise={cr.exercises} />
-          )}
-        </div>
-      )
+        const { parts } = props
+        return (
+            <div>
+                {parts.map((cr, i) =>
+                    <Part key={i} part={cr.name} exercise={cr.exercises} />
+                )}
+            </div>
+        )
     }
 
     const Total = (props) => {
-      const {parts} = props
-      const sum = parts.reduce(
-        (accumulator, currentValue) => accumulator + currentValue.exercises,
-        0,
-      );
-      return (
-        <p>
-            <b>
-                total of {sum} exercises
-            </b>
-        </p>
-      )
+        const { parts } = props
+        const sum = parts.reduce(
+            (accumulator, currentValue) => accumulator + currentValue.exercises,
+            0,
+        );
+        return (
+            <p>
+                <b>
+                    total of {sum} exercises
+                </b>
+            </p>
+        )
     }
-  
+
     return (
-      <div>
-        <Header name={course.name} />
-        <Content parts={course.parts}/>
-        <Total parts={course.parts}/>
-      </div>
+        <div>
+            <h1>Web development curriculum</h1>
+            {courses.map((course, j, k, l) =>
+                <div>
+                    <Header key={j} name={course.name} />
+                    <Content key={k} parts={course.parts} />
+                    <Total key={l} parts={course.parts} />
+                </div>
+            )}
+        </div>
     )
-  }
+}
 
 export default Course
