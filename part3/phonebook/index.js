7468f825f15a3c3bc33f7c4867c720b5bf8e99dc
Yihong Wang <bellowswang@gmail.com>
2023-08-08 22:34:41 +0200

    3.5 and 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -87,30 +89,34 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-
-  if (!body.content) {
+  if (!body.name) {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name missing' 
     })
   }
-
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
   const person = {
-    id: generateId(),
-    content: body.content,
+    id: getRandomInt(10000),
+    name: body.name,
+    number: body.number
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  if (persons.some(p => p.name === person.name)) {
+    response.status(400).json( {error: 'name must be unique'} )
+  } else {
+    persons = persons.concat(person)
+    response.json(person)
+  }
 })
 
 const PORT = 3001
